a3f17719a8a2e90429d1b1277231d2740ded30f1
CyberSeeds-uk <cyberseeds.uk@gmail.com>
2026-01-10 13:43:11 +0000

    Update script.js

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ window.addEventListener("unhandledrejection", e =>
 );
 
 
-document.addEventListener("DOMContentLoaded", () => {
+
   const $ = (sel, root = document) => root.querySelector(sel);
   const $$ = (sel, root = document) => Array.from(root.querySelectorAll(sel));
 
